9c944d2743e45b9b9d549fc426fabecdf74d12fd
Lera2597 <lera25.roas@gmail.com>
2023-06-02 11:05:32 -0500

    Creacion de objetos

--- Archivo1.py
+++ Archivo1.py
@@ -2,3 +2,18 @@
 import numpy as np
 valor = np.sin(np.pi)
 print(f"El valor de seno de pi es :{valor}")
+
+# branch objeto
+
+class vehiculo:
+    def __init__(self,numero_ruedas,numero_puertas):
+        self.Numero_ruedas = numero_ruedas
+        self.Numero_puertas = numero_puertas
+        print("objeto vehiculo creado")
+
+class carro(vehiculo):
+    def __init__(self,numero_ruedas,numero_puertas,marca):
+        super().__init__(numero_ruedas,numero_puertas)
+        self.Marca = marca
+
+mazda = carro(4,4,"mazda")
