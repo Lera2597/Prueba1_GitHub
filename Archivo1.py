393784ad2a8ba50a8800baeae1efe7c199cbf0b2
lera2597 <lera25.roas@gmail.com>
2023-06-04 14:22:03 -0500

    fun: mdf_01

--- Archivo1.py
+++ Archivo1.py
@@ -2,3 +2,10 @@
 import numpy as np
 valor = np.sin(np.pi)
 print(f"El valor de seno de pi es :{valor}")
+
+
+def imprimir(bandera):
+
+    print (f"se imprimio: {'positivo' if bandera else 'negativo'}")
+
+imprimir(True)
